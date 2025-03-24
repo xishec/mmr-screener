d01c3be4393d567dd241263200b29d7909641427
xishec <xishec@gmail.com>
2025-03-24 16:26:02 -0400

    wip

--- relative-strength.py
+++ relative-strength.py
@@ -1,11 +1,12 @@
+import string
+
 import rs_data
 import rs_ranking
-import sys
 
 
 def main():
-    char = None if len(sys.argv) <= 1 else sys.argv[1]
-    rs_data.main(char)
+    for char in string.ascii_lowercase:
+        rs_data.main(char)
     rs_ranking.main(skipEnter="true")
 
 
